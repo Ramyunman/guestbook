a9aedd18f20bf6c4166bc6bb89832fd3e5fa4cd3
Ramyunman <prettyshield@naver.com>
2022-10-14 16:29:02 +0900

    part2 start

--- src/test/java/org/zerock/guestbook/GuestbookApplicationTests.java
+++ src/test/java/org/zerock/guestbook/GuestbookApplicationTests.java
@@ -1,13 +1,17 @@
 package org.zerock.guestbook;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
-class GuestbookApplicationTests {
+public class GuestbookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
